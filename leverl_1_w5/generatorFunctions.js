0eda16638b88e0aa689cefcfed341c46a9173310
Amir Alsayed <amyralsyd367@gmail.com>
2024-10-17 23:07:29 +0300

    generator functions*

--- leverl_1_w5/generatorFunctions.js
+++ leverl_1_w5/generatorFunctions.js
@@ -2,5 +2,77 @@
 // you can execute them and press pause, do something else and tehn come back and continue
 
 function* generatorExample() {
-    
+    console.log("amir");
+
+    yield 8;
+    yield 9;
+    yield 10;
+
+    return 11; // << thiw will be the value wehn we reach the done condition
+}
+
+
+const generator = generatorExample();
+
+console.log(generator.next());
+// here we can do what we want and then we can call the next() to continu
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+
+
+for (const n of generatorExample()) {
+    console.log(n);
+}
+
+
+function* createSquareNumbersGenerator(max) {
+    let n = 0;
+    console.log('start');
+
+
+    while (n < max) {
+        n++;
+        yield n ** 2;
+    }
+
+    return console.log('end');
+}
+
+
+const sq = createSquareNumbersGenerator(5);
+
+
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+console.log(sq.next().value);
+
+
+for (const n of createSquareNumbersGenerator(10)) {
+    console.log(n);
+}
+
+
+// random picker
+
+function* createUniqeRandomGenerator(names) {
+    const available = names;
+    while (available.legth !== 0) {
+        const randI = Math.floor(Math.random() * available.length)
+        const value = available[randI]
+        available.splice(randI, 1)
+        yield value;
+    }
+}
+
+const names = ['amir', 'ahmed', 'mohamed', 'hamada']
+
+const uniqueNames = createUniqeRandomGenerator(names);
+for (const name of uniqueNames) {
+    console.log(name);
 }
