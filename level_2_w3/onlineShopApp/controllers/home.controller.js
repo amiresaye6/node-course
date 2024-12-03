9e80b634437cfdd2761f4591eb2ccd5f341beb5a
Amir Alsayed <amyralsyd367@gmail.com>
2024-12-03 20:25:39 +0200

    final touches

--- level_2_w3/onlineShopApp/controllers/home.controller.js
+++ level_2_w3/onlineShopApp/controllers/home.controller.js
@@ -1,29 +1,38 @@
 const productsModel = require("../models/products.model")
 
-const getHome = (req, res) => {
-    // get data
-    // render home page
-    let productsPromise;
-    const validCategories = ["men", "women", "both"]
-    const category = req.query.category
-    if (category && validCategories.includes(category)) {
-        productsPromise = productsModel.getProductsByCategory(category)
-    } else {
-        productsPromise = productsModel.getAllProducts()
+const getHome = async (req, res) => {
+    try {
+        // Get category from query
+        const category = req.query.category
+        
+        // Fetch products based on category
+        const products = category 
+            ? await productsModel.getProductsByCategory(category)
+            : await productsModel.getAllProducts()
+        
+        // Get unique categories
+        const allProducts = await productsModel.getAllProducts()
+        const uniqueCategories = [...new Set(
+            allProducts
+                .map(product => product.category?.toLowerCase())
+                .filter(Boolean)
+        )].sort()
+
+        res.render("index", {
+            products,
+            uniqueCategories,
+            isUser: req.session.userId,
+            isAdmin: req.session.isAdmin,
+            validationErrors: req.flash('validationErrors')
+        })
+    } catch (err) {
+        res.json(err)
     }
-    productsPromise.then(products => res.render("index", {
-        products,
-        isUser: req.session.userId,
-        isAdmin: req.session.isAdmin,
-        validationErrors: req.flash('validationErrors')
-    }))
-    .catch(err => res.json(err));
 }
 
 const getLndingPage = (req, res) => {
     res.render('landingPage')
 }
-
 
 module.exports = {
     getHome,
